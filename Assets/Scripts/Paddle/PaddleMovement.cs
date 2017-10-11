afd4800de9b61bd65adde1d7ceb6e6f317e2f9b7
rkmoon <rkmoon03@gmail.com>
2017-10-11 17:48:25 -0500

    Added Code
    Paddle and ball code

--- Assets/Scripts/Paddle/PaddleMovement.cs
+++ Assets/Scripts/Paddle/PaddleMovement.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 0.1f;
+    public GameObject ball;
+    float movePaddleAmount = 0;
+    Collider2D coll;
+    Collider2D ballColl;
+    SpriteRenderer ballSprite;
+    bool ballInPlay = false;
+    float ballY;
+
+    // Use this for initialization
+    void Start () {
+        coll = this.GetComponent(typeof(Collider2D)) as Collider2D;
+
+        ball = Instantiate(Resources.Load("Ball", typeof(GameObject)), new Vector3(5, 5, 5), new Quaternion()) as GameObject;
+        Debug.Log(ball.transform.position);
+        ballColl = ball.GetComponent(typeof(Collider2D)) as Collider2D;
+        ballSprite = ball.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        MovePaddle();    
+        if (!ballInPlay)
+        {
+            AttachBallToPaddle();
+        }
+        if(Input.GetAxis("Jump") > 0)
+        {
+            LaunchBall();
+        }
+        
+        
+
+           
 	}
+    void MovePaddle()
+    {
+        movePaddleAmount = Input.GetAxis("Horizontal") * speed;
+        //Check bounds of Level
+        if (coll.bounds.min.x > -8 && movePaddleAmount < 0)
+        {
+            this.transform.Translate(movePaddleAmount, 0, 0);
+        }
+        else if (coll.bounds.max.x < 8 && movePaddleAmount > 0)
+        {
+            this.transform.Translate(movePaddleAmount, 0, 0);
+        }
+
+    }
+
+    void AttachBallToPaddle()
+    {
+        //Debug.Log("min: " + ballColl.bounds.min.y + "max: " + ballColl.bounds.max.y);
+        ballY = coll.bounds.max.y + (ballColl.transform.position.y - ballColl.bounds.min.y);//(ballSprite.size.y / 2);
+
+        ball.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, ballY, 0), new Quaternion());
+    }
+
+    void LaunchBall()
+    {
+
+    }
 }
